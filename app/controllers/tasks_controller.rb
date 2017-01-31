06067ebeb89b73b14c8724f62d6a8cc205de5c8c
kbanico <code4uguam@gmail.com>
2017-01-31 23:00:26 +1000

    change redirect to tasks_path after edit update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
